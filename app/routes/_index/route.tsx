f3b9104b373b8b0124c0aef54f3d24feba2955bb
Amarjeet Choudhary <amarjeetchoudhary647@gmail.com>
2025-12-09 13:11:32 +0530

    fixes

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,20 +1,9 @@
-import { redirect, Form, useLoaderData } from "react-router";
-import type { LoaderFunctionArgs } from "react-router";
-import { login } from "../../shopify.server";
+import { Form } from "react-router";
 import styles from "./style.module.css";
 
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const url = new URL(request.url);
-
-  if (url.searchParams.get("shop")) {
-    throw redirect(`/app?${url.searchParams.toString()}`);
-  }
-
-  return { showForm: Boolean(login) };
-};
-
 export default function App() {
-  const { showForm } = useLoaderData();
+  // Always show the form on the landing page
+  const showForm = true;
   
 
   return (
